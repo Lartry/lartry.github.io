6c1edc4c2b3e4760668403a3358bfca84823ee68
lartry <wl.lartry@gmail.com>
2019-03-31 22:56:54 +0800

    Site updated: 2019-03-31 22:56:51

--- dist/music.js
+++ dist/music.js
@@ -3,16 +3,10 @@ const ap = new APlayer({
     fixed: true,
     autoplay: true,
     audio: [{
-            name: "PDD洪荒之力",
-            artist: '徐梦圆',
-            url: 'http://up.mcyt.net/?down/39868.mp3',
+            name: "归来",
+            artist: '满江',
+            url: 'http://lartry.github.io/归来 - 满江.flac',
             cover: 'http://oeff2vktt.bkt.clouddn.com/image/84.jpg',
-        },
-        {
-            name: '9420',
-            artist: '麦小兜',
-            url: 'http://up.mcyt.net/?down/45967.mp3',
-            cover: 'http://oeff2vktt.bkt.clouddn.com/image/8.jpg',
         },
         {
             name: '风筝误',
