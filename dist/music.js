6e3fb9e02345936db798e77173e5b2e65023fd63
lartry <wl.lartry@gmail.com>
2019-03-31 19:57:12 +0800

    Site updated: 2019-03-31 19:57:11

--- dist/music.js
+++ dist/music.js
@@ -1,7 +1,7 @@
 const ap = new APlayer({
     container: document.getElementById('aplayer'),
     fixed: true,
-    autoplay: false,
+    autoplay: true,
     audio: [{
             name: "PDD洪荒之力",
             artist: '徐梦圆',
