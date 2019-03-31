50bdab5d85df82b47462e3a21f214183db084f10
lartry <wl.lartry@gmail.com>
2019-04-01 07:03:53 +0800

    Site updated: 2019-04-01 07:03:53

--- dist/music.js
+++ dist/music.js
@@ -5,14 +5,14 @@ const ap = new APlayer({
     audio: [{
             name: "归来",
             artist: '满江',
-            url: 'http://lartry.github.io/归来 - 满江.flac',
-            cover: 'http://oeff2vktt.bkt.clouddn.com/image/84.jpg',
+            url: '/mp3/归来 - 满江.flac',
+            cover: '/img/logo.png',
         },
         {
             name: '风筝误',
             artist: '刘珂矣',
             url: 'http://up.mcyt.net/?down/46644.mp3',
-            cover: 'http://oeff2vktt.bkt.clouddn.com/image/96.jpg',
+            cover: '/img/logo.png',
         }
     ]
 });
